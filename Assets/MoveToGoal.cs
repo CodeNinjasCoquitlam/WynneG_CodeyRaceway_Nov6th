caaeb2997023bbf2491065c644b5be96203bd17a
CodeNinjasCoquitlam <coquitlamninja@gmail.com>
2025-02-24 19:47:58 -0800

    Feb21

--- Assets/MoveToGoal.cs
+++ Assets/MoveToGoal.cs
@@ -10,13 +10,23 @@ public class MoveToGoal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        obstacle = GameObject.FindGameObjectWithTag("obstacle");
-        agent = GetComponent<NavMeshAgent>();
-        agent.destination = obstacle.transform.position;
+
     }
     // Update is called once per frame
     void Update()
     {
-        
+        obstacle = GameObject.FindGameObjectWithTag("obstacle");
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = obstacle.transform.position;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "obstacle")
+        {
+            print("Test!");
+            Destroy(other.gameObject);
+            Destroy(transform.gameObject);
+        }
     }
 }
